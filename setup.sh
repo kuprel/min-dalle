8b5960b68706cec1eecfedcd896b5d42463ca94e
Brett Kuprel <brkuprel@gmail.com>
2022-07-01 07:54:19 -0400

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,6 +10,6 @@ mkdir -p pretrained/vqgan
 curl https://huggingface.co/dalle-mini/vqgan_imagenet_f16_16384/resolve/main/flax_model.msgpack -L --output ./pretrained/vqgan/flax_model.msgpack
 
 # download dalle-mini and dalle mega
-wandb login --anonymously
-wandb artifact get --root=./pretrained/dalle_bart_mini dalle-mini/dalle-mini/mini-1:v0
-wandb artifact get --root=./pretrained/dalle_bart_mega dalle-mini/dalle-mini/mega-1-fp16:v14 
+python -m wandb login --anonymously
+python -m wandb artifact get --root=./pretrained/dalle_bart_mini dalle-mini/dalle-mini/mini-1:v0
+python -m wandb artifact get --root=./pretrained/dalle_bart_mega dalle-mini/dalle-mini/mega-1-fp16:v14 
