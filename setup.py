2f7f674a252b97c85c58d57610e25af86dc426ec
kamalkraj <kamalraj97@gmail.com>
2022-07-03 22:17:20 +0530

    add requirements to setup and fix file name

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 import setuptools
 from pathlib import Path
 
+requirements = Path("requirements.txt").read_text().splitlines()
+
 setuptools.setup(
     name='min-dalle',
     description = 'min(DALL·E)',
-    long_description=(Path(__file__).parent / "README").read_text(),
+    long_description=(Path(__file__).parent / "README.md").read_text(),
     version='0.2.10',
     author='Brett Kuprel',
     author_email='brkuprel@gmail.com',
@@ -17,7 +19,7 @@ setuptools.setup(
     install_requires=[
         'torch>=1.10.0',
         'typing_extensions>=4.1.0'
-    ],
+    ] + requirements,
     keywords = [
         'artificial intelligence',
         'deep learning',
