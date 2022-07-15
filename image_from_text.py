ee770871f7f100dd38e01b06f0ac7bb3646d2f25
Jonas Konschak <jonaskonschak@gmx.de>
2022-07-15 21:15:02 +0200

    improve cli

--- image_from_text.py
+++ image_from_text.py
@@ -1,20 +1,38 @@
 import argparse
 import os
+import torch
 from PIL import Image
 from min_dalle import MinDalle
-import torch
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--mega', action='store_true')
-parser.add_argument('--no-mega', dest='mega', action='store_false')
-parser.set_defaults(mega=False)
-parser.add_argument('--fp16', action='store_true')
-parser.add_argument('--text', type=str, default='Dali painting of WALL·E')
-parser.add_argument('--seed', type=int, default=-1)
-parser.add_argument('--grid-size', type=int, default=1)
-parser.add_argument('--image-path', type=str, default='generated')
-parser.add_argument('--models-root', type=str, default='pretrained')
-parser.add_argument('--top_k', type=int, default=256)
+
+parser = argparse.ArgumentParser('min-dalle',     
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--mega', action='store_true',
+    help='use mega model')
+parser.add_argument('--fp16', action='store_true',
+    help='use FP16')
+parser.add_argument('--bf16', action='store_true',
+    help='use BF16 (Ampere only)')
+parser.add_argument('--quiet', action='store_true', 
+    help='quiet mode')
+parser.add_argument('--print', action='store_true',
+    help='print generated image as ascii art')
+parser.add_argument('--text', type=str, default='Dali painting of WALL·E',
+    help='text prompt (str)')
+parser.add_argument('--seed', type=int, default=-1,
+    help='seed for random number generator, set to negative for random seed (int)')
+parser.add_argument('--grid-size', type=int, default=1, 
+    help='grid size, will generate grid_size * grid_size images (int)')
+parser.add_argument('--image-path', type=str, default='generated',
+    help='path to save image to (str)')
+parser.add_argument('--models-root', type=str, default='pretrained',
+    help='path to pretrained models (str)')
+parser.add_argument('--top_k', type=int, default=256,
+    help='top k parameter for generation (int)')
+parser.add_argument('--temp', type=float, default=1.0,
+    help='temperature for generation (float)')
+parser.add_argument('--supercond', type=int, default=16,
+    help='supercondition factor for generation (int)')
 
 
 def ascii_from_image(image: Image.Image, size: int = 128) -> str:
@@ -30,7 +48,7 @@ def save_image(image: Image.Image, path: str):
         path = os.path.join(path, 'generated.png')
     elif not path.endswith('.png'):
         path += '.png'
-    print("saving image to", path)
+    print('saving image to', path)
     image.save(path)
     return image
 
@@ -40,33 +58,45 @@ def generate_image(
     text: str,
     seed: int,
     grid_size: int,
+    temperature: float,
     top_k: int,
     image_path: str,
     models_root: str,
-    fp16: bool,
+    dtype:torch.dtype,
+    supercondition_factor: int,
+    is_verbose: bool,
+    print_ascii: bool,
 ):
     model = MinDalle(
         is_mega=is_mega, 
         models_root=models_root,
         is_reusable=False,
         is_verbose=True,
-        dtype=torch.float16 if fp16 else torch.float32
+        dtype=dtype
     )
 
     image = model.generate_image(
         text, 
         seed, 
         grid_size, 
+        temperature=temperature,
         top_k=top_k, 
-        is_verbose=True
+        supercondition_factor=supercondition_factor,
+        is_verbose=is_verbose
     )
     save_image(image, image_path)
-    print(ascii_from_image(image, size=128))
+    if print_ascii: print(ascii_from_image(image, size=128))
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    print(args)
+    print('Config:', vars(args))
+    if args.fp16:
+        dtype = torch.float16
+    if args.bf16:
+        dtype = torch.bfloat16
+    else:
+        dtype = torch.float32
     generate_image(
         is_mega=args.mega,
         text=args.text,
@@ -75,5 +105,9 @@ if __name__ == '__main__':
         top_k=args.top_k,
         image_path=args.image_path,
         models_root=args.models_root,
-        fp16=args.fp16,
+        dtype=dtype,
+        temperature=args.temp,
+        supercondition_factor=args.supercond,
+        is_verbose=not args.quiet,
+        print_ascii=args.print,
     )
